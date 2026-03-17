51ec60c6e7b8515fbcfcbe6a561277365f147598
Manish <Manishjha12674@gmail.com>
2026-03-17 14:49:18 +0530

    Force admin creation in wsgi

--- exam_system/exam_system/wsgi.py
+++ exam_system/exam_system/wsgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_system.settings')
 
 application = get_wsgi_application()
+
+User = get_user_model()
+
+try:
+    user, created = User.objects.get_or_create(username="admin")
+    user.set_password("admin123")
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    print("✅ Admin ensured")
+except Exception as e:
+    print("Admin creation error:", e)
