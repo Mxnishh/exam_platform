6224fa18f6dc3a9b01086771c38ed11511329c95
Manish <Manishjha12674@gmail.com>
2026-03-20 12:08:23 +0530

    Fix admin creation logic

--- exam_system/exam_system/wsgi.py
+++ exam_system/exam_system/wsgi.py
@@ -1,29 +1,6 @@
-"""
-WSGI config for exam_system project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_system.settings')
 
 application = get_wsgi_application()
-
-User = get_user_model()
-
-try:
-    user, created = User.objects.get_or_create(username="admin")
-    user.set_password("admin123")
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
-    print("✅ Admin ensured")
-except Exception as e:
-    print("Admin creation error:", e)
